7affe21c26c8cf4731a4aecbbc1ca2dfbbe5ead9
Julio Cezar <jc.2013oliveira@gmai.com>
2018-02-25 23:25:46 -0300

    jokenpo project

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+import unittest
+from .sample.jogada
+
+class TestJokenpo(unittest.TestCase):
+    def test_jogada(self):
+        pass
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
