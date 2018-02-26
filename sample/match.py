9625c2cea64368d8bc16a71af17a803e0435e722
Julio Cezar <jc.2013oliveira@gmai.com>
2018-02-26 00:19:33 -0300

    jokenpo

---
+++ sample/match.py
@@ -0,0 +1,19 @@
+import playermove
+
+class match():
+
+
+    def winner_loser(self):
+        self.player_
+        if (self.'Paper' && 'Stone'):
+            self.winner = 'Paper'
+            self.loser = 'Stone'
+            return self
+
+        if ('Scissor' && 'Paper'):
+            self.winner = 'Scissor'
+            self.loser = 'Paper'
+
+        if ( 'Stone' &&'Scissor'):
+            self.winner = 'Stone'
+            self.loser = 'Scissor'
---
+++ sample/playermove.py
@@ -0,0 +1,17 @@
+class PlayerMove():
+
+
+    def choose_stone(self):
+        self.move = 'Stone'
+        return self.move
+
+    def choose_scissor(self):
+        self._scissor = 'Scissor'
+        return self.move
+
+    def choose_paper(self):
+        self.move = 'Paper'
+        return self.move
+
+    def getmove(self):
+        return self.move
